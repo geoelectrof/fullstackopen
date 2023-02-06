18f538f31207d9c4d28fd9ce6fb7cc139bcb06a2
Dimitris Georgakopoulos <dimitris.geo@gmail.com>
2023-02-06 20:51:18 +0200

    part1 - anecdotes - step3 enhansments

--- part-1/anecdotes/src/App.js
+++ part-1/anecdotes/src/App.js
@@ -1,5 +1,18 @@
 import { useState } from 'react'
 
+const Anecdote = (props) => {
+  return (
+    <>
+      <div>
+        {props.text}
+      </div>
+      <div>
+        has {props.votes} votes
+      </div>
+    </>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -14,6 +27,7 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array(7).fill(0))
+  const [mostVotes, setMostVotes] = useState(0)
 
   const generateRandomAnecdote = () => {
     const randomValue = Math.floor(Math.random() * 7)
@@ -24,29 +38,24 @@ const App = () => {
     const copy = [...votes]
     copy[selectedAnecdote] += 1
     setVotes(copy)
+    setMostVotes(copy.indexOf(Math.max(...copy))) 
   }
 
-  const mostVotes = () => {
-    return votes.indexOf(Math.max(...votes))
-  }
-
-  console.log(mostVotes())
 
   return (
     <>
       <h1>Anecdote of the day</h1>
-      <div>
-        {anecdotes[selected]}
-      </div>
-      <div>
-        has {votes[selected]} votes
-      </div>
+      <Anecdote 
+        text = {anecdotes[selected]}
+        votes = {votes[selected]}
+      />
       <button onClick={()=>addVote(selected)}>vote</button>
       <button onClick={generateRandomAnecdote}>next anecdote</button>
       <h1>Anecdote with most votes</h1>
-      <div>
-        {anecdotes[mostVotes()]}
-      </div>
+      <Anecdote
+        text = {anecdotes[mostVotes]}
+        votes = {votes[mostVotes]}
+      />
     </>
   )
 }
