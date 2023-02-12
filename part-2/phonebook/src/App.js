438625dfe22a4d68d51f6e1d79b58e8c7869e6e7
Dimitris Georgakopoulos <dimitris.geo@gmail.com>
2023-02-12 13:14:04 +0200

    part2 - phonebook - step4 completed

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: 123456789
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setnewSearch] = useState('')
 
   const handleSubmit = (event) => {
     const personsArr = persons.map(person => person.name)
@@ -26,9 +27,29 @@ const App = () => {
     }
   }
 
+  const handleNewSearch = (event) => {
+    setnewSearch(event.target.value)
+  }
+
+  const displayPersons = () => {
+    const regex = new RegExp(newSearch, 'gi')
+    const newArr = persons.filter( person => {
+      return person.name.match(regex)
+    })
+    return newArr
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with
+        <input 
+          value = {newSearch}
+          onChange={(event) => handleNewSearch(event)}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={(event) => handleSubmit(event)}>
         <div>name: 
           <input 
@@ -47,9 +68,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {/* {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)} */}
+      {displayPersons().map(person => <div key={person.name}>{person.name} {person.number}</div>)}
       <br />
-      <div>debug: {newName} {newNumber}</div>
     </div>
   )
 }
